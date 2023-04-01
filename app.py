b24cbc8d015a5a0ee099e41dfdf92d147b3fdb72
Alan-RiBas <alanbatista.ribas@gmail.com>
2023-04-01 16:26:31 -0300

    Base do Projeto

---
+++ app.py
@@ -0,0 +1,18 @@
+import pygame
+import random 
+import os
+
+TELA_ALTURA = 800
+TELA_LARGURA = 500
+
+IMAGE_CANO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'pipe.png')))
+IMAGE_CHAO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'base.png')))
+IMAGE_BACKGROUND = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bg.png')))
+IMAGE_PASSARO = [
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird1.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird2.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird3.png')))
+]
+
+pygame.font.init()
+FONTE_PONTOS = pygame.font.SysFont('arial', 50)
